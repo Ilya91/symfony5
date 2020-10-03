cf5ede04f145983098ee5a0320aa5ff66abe523b
ilya-cactus <ilya.mironchikov@cactussoft.biz>
2020-10-03 19:47:03 +0300

    admin template

---
+++ app/assets/admin/admin.js
@@ -0,0 +1,8 @@
+import './css/admin.scss';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+const $ = require('jquery');
+require('bootstrap');
+import './js/custom';
+
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -23,7 +23,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/app/app.js')
+    .addEntry('admin', './assets/admin/admin.js')
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
 
@@ -64,7 +65,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
