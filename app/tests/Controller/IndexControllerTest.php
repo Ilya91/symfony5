5c13cb6083413e0facce50c02f86fb7e5e416d26
ilya-cactus <ilya.mironchikov@cactussoft.biz>
2020-10-26 21:59:31 +0300

    relations

---
+++ app/tests/Controller/IndexControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('h1', 'Shop Name');
+    }
+}
---
+++ app/tests/Entity/PostTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class PostTest extends TestCase
+{
+    public function testPost()
+    {
+        $post = new Post();
+        $this->assertSame(true);
+    }
+
+}
