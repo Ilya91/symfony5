8def01eb6fb0d489c2fecb14ee68a8ffc6d220f7
ilya-cactus <ilya.mironchikov@cactussoft.biz>
2020-10-27 16:26:57 +0300

    elasticsearch configuration

--- app/src/Controller/Admin/AdminController.php
+++ app/src/Controller/Admin/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use Elasticsearch\ClientBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -15,5 +17,40 @@ class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    /**
+     * @Route("/admin/test", name="test")
+     */
+    public function test()
+    {
+        $hosts = [
+            'elasticsearch', // elastic host was added to you hosts file automatically
+        ];
+        $client = ClientBuilder::create()->setHosts($hosts)->build();
+
+        $params = [
+            'index' => 'my_index',
+            'id'    => 'my_id',
+            'body'  => ['testField' => 'abc']
+        ];
+
+        $client->index($params);
+
+        $params = [
+            'index' => 'my_index',
+            'body'  => [
+                'query' => [
+                    'match' => [
+                        'testField' => 'abc'
+                    ]
+                ]
+            ]
+        ];
+
+        $response = $client->search($params);
+        return new Response(
+            dd($response)
+        );
     }
 }
